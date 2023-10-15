b0b920a0e791ac13ff9d97859ff8d211eaa118ce
Vladislav <vladislav_gabaraev@mail.ru>
2023-10-15 21:53:49 +0300

    added green background

--- Alhpa_ios_home/ViewController.swift
+++ Alhpa_ios_home/ViewController.swift
@@ -12,7 +12,7 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemGreen
     }
 
 
